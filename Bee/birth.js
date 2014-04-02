cabd6ea49f0d49cda3343fda26ebb696e19f75be
root <root@temper0.(none)>
2014-04-02 17:55:00 -0400

    Updated a lot for installation

--- Bee/birth.js
+++ Bee/birth.js
@@ -4,12 +4,12 @@ var fs = require('fs')
 require('getmac').getMac(function(err,macAddress){
   if (err)  throw err;
   fs.writeFile(__dirname + "/macaddress", macAddress, function(err) {
+    console.log(macAddress)
     if(err) return console.log(err)
   })
   request({ "url": "http://127.0.0.1:125/egg/new", method: "POST", json: {"sensors": ["0x02"], "address": macAddress}}, function(err, response, body) {
     if(err) return console.log(err)
-    console.log(response)
-    console.log(body)
+    return console.log(response)
   })     
 });
 
---
+++ README.md
@@ -0,0 +1,29 @@
+
+
+
+# Install
+
+
+## install dependency of Hive for data logging 
+
+git clone Hive to /root/Hive
+
+Follow directions in /root/Hive/README.md
+
+
+## install of Fido 
+
+```
+npm install -g forever;
+cd /root;
+git clone https://github.com/rjsteinert/Fido.git; 
+echo "tmpfs /srv/tmp tmpfs rw,nosuid,noexec,relatime,size=1024k,mode=755 0 0" >> /etc/fstab;
+sudo ln -s /usr/local/bin/node /usr/bin/node;
+cp /root/Fido/util/init.d/node /etc/init.d/node;
+cd /root/Fido;
+npm install;
+node /root/Fido/Bee/birth.js;
+
+
+
+
