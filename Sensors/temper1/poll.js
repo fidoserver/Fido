b4df7c8de5df227b9eabd46794e5656a4a81f0cc
R.J. Steinert <rj@rjsteinert.com>
2014-07-18 04:59:21 +0000

    Using temper-python for temper1 sensor polling

--- Sensors/temper1/poll.js
+++ Sensors/temper1/poll.js
@@ -2,15 +2,19 @@ var fs = require('fs')
 var l = require('../../lib/log.js')
 l.context = __filename 
 
-var thermometers=require("temper1")
-var devices=thermometers.getDevices()
-if (!devices || devices.length == 0) return l.g('No devices found')
-
-thermometers.readTemperature(devices[0], function(err, value) {
-  if(err) return l.g(err)
+var sys = require('sys')
+var exec = require('child_process').exec;
+function puts(error, stdout, stderr) { console.log(stdout) }
+var cmd = '/usr/local/bin/temper-poll'
+exec(cmd, function(err, stdout, stderr) { 
+  var start = stdout.indexOf(':') + 2
+  var end = stdout.indexOf('C') - 1
+  //console.log(start)
+  //console.log(end)
+  var value = stdout.substr(start, end - start)
+  //console.log(value)
   fs.writeFile("/srv/tmp/temper1", value, function(err) {
     if(err) return l.g(err)
-    console.log('wrote ' + value)
   })
 })
 
