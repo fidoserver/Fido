0ad3baf393e5f2fe5371481823093b97a3eca1f9
R.J. Steinert <rj@rjsteinert.com>
2014-07-18 05:10:57 +0000

    Cleanup and README docs

--- Sensors/temper1/poll.js
+++ Sensors/temper1/poll.js
@@ -9,10 +9,7 @@ var cmd = '/usr/local/bin/temper-poll'
 exec(cmd, function(err, stdout, stderr) { 
   var start = stdout.indexOf(':') + 2
   var end = stdout.indexOf('C') - 1
-  //console.log(start)
-  //console.log(end)
   var value = stdout.substr(start, end - start)
-  //console.log(value)
   fs.writeFile("/srv/tmp/temper1", value, function(err) {
     if(err) return l.g(err)
   })
